2ad5b052d9d7b2052f373bad973965f4b48ad56f
simonccc <simonc@gmail.com>
2025-10-26 19:46:10 +0000

    fixes

---
+++ puppet-r10k/install_runner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+cd /home/github
+
+if [ "$1" = "" ]; then
+  echo "no target passed"
+  exit
+fi
+
+if [ "$1" = "linux/amd64"]; then
+  TARGET=x64
+else
+  TARGET=arm64
+fi
+
+VERSION=$(curl --silent "https://api.github.com/repos/actions/runner/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed s/^v//)
+
+printf "$TARGET $VERSION"
+
+curl -O -L https://github.com/actions/runner/releases/download/v$VERSION/actions-runner-linux-$TARGET-$VERSION.tar.gz  
+tar xzf ./actions-runner-linux-*-*.tar.gz
+./bin/installdependencies.sh 
+chown -R github * 
